9530d79bf6d3614c9a7765c02e6d23f5cc72296f
shaunleong <35859746+shaunleong@users.noreply.github.com>
2018-08-14 11:41:58 +0800

    Add files via upload

--- SSD1306_OLED.cpp
+++ SSD1306_OLED.cpp
@@ -27,7 +27,7 @@ namespace OLED {
 	
 	//%
     void showString(StringData *text) {
-		oled->printf("%s $$$$$$$$$\n", text->data);
+		oled->printf("%s $$\n", text->data);
 		oled->display();
     }
     
